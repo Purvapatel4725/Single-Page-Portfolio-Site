cc8e8e3457aa920db579a9cd1b7d35a58ba9330e
Purvapatel4725 <144143336+Purvapatel4725@users.noreply.github.com>
2023-11-02 13:00:05 -0400

    Added partials folder segregation

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
-app.use(express.static(path.join(__dirname, 'views')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
